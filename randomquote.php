07f25120da8293f83dd12cd4068c76508788b47c
Joshua Thomas <joshua.fujishima@gmail.com>
2017-02-21 19:01:36 -0500

    Fixed the alignment of the button and author information.

--- randomquote.php
+++ randomquote.php
@@ -12,6 +12,8 @@
   <link rel="stylesheet" href="css/theme.css">
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <!-- <body id="backdrop" class="img-responsive">
 
@@ -29,7 +31,7 @@
       
       <div class="row">
         <div id ="quote" class="col-md-12 col-centered">
-        THERE'S STUFF HERE!
+        
         </div>
       
         <div class="row">
@@ -39,7 +41,7 @@
         </div>
 
         <div class="row">
-          <button id="newquote">New Quote</button>
+          <button id="newquote" class="btn btn-default">New Quote</button>
         </div>
                   
       </div>
