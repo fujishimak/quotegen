599c3678324e6453b0b92ffc08e237991513560d
Joshua Thomas <joshua.fujishima@gmail.com>
2017-02-24 17:50:46 -0500

    Completed primary release.

--- js/functions.js
+++ js/functions.js
@@ -5,27 +5,15 @@ function doIt() {
     data: {}, // Additional parameters here
     dataType: 'json',
     success: function(data) {
-    	//
-        //Change data.source to data.something , where something is whichever part of the object you want returned.
-        //To see the whole object you can output it to your browser console using:
-        //console.log(data);
-/*        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);*/
-
-
-       	/*document.getElementById("output").innerHTML =  data.quote + "\n-- " + data.author + ". " + w + "x" + h;
-        document.getElementById("backdrop").style.backgroundImage = "url('https://source.unsplash.com/category/nature/1600x900')"; */
-
-/*        document.getElementByID("quote").html = "<h1><i class='fa fa-quote-left' aria-hidden='true'></i> " + data.quote + "</h1>";
-      
-        document.getElementByID("author").html = "<h3><i class='fa fa-pencil' aria-hidden='true'></i><em> " + data.author + "</h3>";*/
-
-        // document.getElementById("backdrop").style.backgroundImage = "url('https://source.unsplash.com/random/1600x900')";
+    	
 
         document.getElementById("quote").innerHTML =  "<h1><i class='fa fa-quote-left' aria-hidden='true'></i> " + data.quote + "</h1>";
       
         document.getElementById("author").innerHTML = "<h3><i class='fa fa-pencil' aria-hidden='true'></i><em> " + data.author + "</h3>";
 
+        // document.getElementById("tweet").data-text = data.quote + " --" + data.author;
+
+        document.getElementById("tweet").href = "https://twitter.com/intent/tweet?text=" + data.quote + "%20--" + data.author + "&hashtags=" + "coolquotes";
         },
     error: function(err) { alert(err); },
     beforeSend: function(xhr) {
@@ -34,51 +22,44 @@ function doIt() {
 });
 
 
- /*var output = $.ajax({
-    url: 'https://source.unsplash.com/category/nature/1600x900', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) {
-        //
-        //Change data.source to data.something , where something is whichever part of the object you want returned.
-        //To see the whole object you can output it to your browser console using:
-        //console.log(data);
-        document.getElementById("output").innerHTML =  data.quote + "\n-- " + data.author; 
-        },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "lVCCMNcV04mshM6VhRuhyWjyf5oFp1XCVpUjsndSIVCM8AyGPh"); // Enter here your Mashape key
-    }
-});*/
- 
-
 }
 
 
 function doPic(){
 
     var sel = 0;
-    var category = ["buildings", "nature", "objects", "technology"];
+    var category = ["buildings", "nature", "objects"];
 
-    sel = Math.ceil(Math.random()*3);
+    sel = Math.floor(Math.random()*3);
 
-    /*Preload new backgroundImage. Animate background image swap using jQuery animate()*/
+    // var nextUrl = "url('https://source.unsplash.com/category/" + category[sel] + "/1600x900')";
 
-    var next = new Image();
+    document.getElementById("backdrop").style.backgroundImage = "url('https://source.unsplash.com/category/" + category[sel] + "/1600x900')";
+    // $(backdrop).animate({
+    //     opacity: 0.5,   
+    //     backgroundImage: ""
+    // }, 1000);
 
-    next.src = "https://source.unsplash.com/category/" + category[sel] + "/1600x900";
 
-    document.getElementById("backdrop").style.backgroundImage = "";
-    // $("backdrop").animate({background-image: url(' + next.src + ')}, 1000, function(){});
-
-    document.getElementById("backdrop").style.backgroundImage = "url('https://source.unsplash.com/category/" + category[sel] + "/1600x900')"; 
 }
 
+
+/*function fadeBg(){
+
+// fadeout the background
+
+} 
+
+
+function fadeText(){
+
+
+}
+*/
 //Check if DOM is safe to manipulate, run quote function and add event handler to buttons. $(document).ready is really deprecated...$(function(){...}); is now recommended. 
 $(document).ready(function(){
 
-    //doPic();
+    
     doIt();
     
     document.getElementById("newquote").addEventListener("click", doIt, true);
--- randomquote.php
+++ randomquote.php
@@ -7,23 +7,43 @@
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
  
   <meta charset="utf-8">
-  <title>Mashape Query</title>
+  <title>Random Quote Generator</title>
   
   <link rel="stylesheet" href="css/theme.css">
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+
+  <script src="js/functions.js"></script>
 </head>
-<!-- <body id="backdrop" class="img-responsive">
 
-  <button onclick="doIt()">Run the request</button>
-  <div id="output">The API request is:</div>
-
-  <button id="newquote">Run the request</button>
-  <div id="output">The API request is:</div> -->
 
   <body id="backdrop">
+
+
+
   <div id="wrapper">
     
     
@@ -35,21 +55,27 @@
         </div>
       
         <div class="row">
-          <div id="author" class="col-md-6 pull-right text-right">            
+          <div id="author" class="col-md-8 pull-right text-right">            
         
           </div>
         </div>
 
         <div class="row">
-          <button id="newquote" class="btn btn-default">New Quote</button>
+          <div class="col-md-6 pull-left text-left">
+            <a id="tweet" class="twitter-share-button" href="https://twitter.com/share" target="_blank"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+          </div>
+          <div class="col-md-6 pull-right text-right">
+            <button id="newquote" class="btn btn-default">New Quote</button>
+          </div>
         </div>
                   
       </div>
     </div>  
   
   </div>
+
+ 
   
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-  <script src="js/functions.js"></script>
+
 </body>
 </html>
